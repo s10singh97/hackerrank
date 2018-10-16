67b2a168ed5ab903d3f1ed842fac9bc25e56e727
s10singh97 <s10singh97@yahoo.in>
2018-10-16 23:31:39 +0530

    Lists

---
+++ 1.py
@@ -0,0 +1,14 @@
+#Lists
+
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+aa = []; dd = []
+for i in range(0, x+1):
+    for j in range(0, y+1):
+        for k in range(0, z+1):
+            if (i+j+k) != n:
+                bb = [i, j, k]
+                aa.append(bb)
+print(aa)
